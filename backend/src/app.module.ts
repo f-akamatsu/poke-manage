082c026cfd7c84df63724fb885a90dba25d5aa9c
f-akamatsu <fuhito615@gmail.com>
2023-09-02 22:57:44 +0900

    不要なモジュール削除

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -15,7 +13,5 @@ import { PokemonModule } from './pokemon/pokemon.module';
     }),
     PokemonModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
