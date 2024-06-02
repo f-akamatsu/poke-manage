641f2fa6aa16352e6e0d8c5c97b58b858fa7124f
f-akamatsu <fuhito615@gmail.com>
2024-06-02 15:25:05 +0900

    色々やりなおし

--- apps/api/src/common/exception/illegalArgumentException.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 不正な引数エラー
- */
-export class IllegalArgumentError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
--- apps/api/src/service/move/type/move.type.ts
+++
@@ -1,4 +0,0 @@
-export type MoveType = {
-  moveId: string;
-  name: string;
-};
