2acb0e38cd43c4459a1548a5fb6f3b8d009ba021
f-akamatsu <fuhito615@gmail.com>
2024-06-23 17:42:27 +0900

    EventStoreModule

--- apps/pokemon-service/src/app.module.ts
+++ apps/pokemon-service/src/app.module.ts
@@ -3,18 +3,14 @@ import { PokemonQueryModule } from './queries/pokemon/pokemon-query.module';
 import { CustomCqrsModule } from './common/custom-cqrs.module';
 import { PokemonModule } from './commands/pokemon/pokemon.module';
 import { PrismaModule } from './common/prisma/prisma.module';
-import { EventNestMongoDbModule } from '@event-nest/mongodb';
+import { EventStoreModule } from './common/event-store/event-store.module';
 
 @Module({
   imports: [
     // 共通
     CustomCqrsModule,
     PrismaModule,
-    EventNestMongoDbModule.register({
-      connectionUri: 'mongodb://user:password@localhost:27017/pokemon',
-      aggregatesCollection: 'aggregates-collection',
-      eventsCollection: 'events-collection',
-    }),
+    EventStoreModule,
     // Queries
     PokemonQueryModule,
     // Commands
