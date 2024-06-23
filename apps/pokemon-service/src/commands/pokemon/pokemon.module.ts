4090a679f1eeec14ba02c80affd50a29a9dec3c2
f-akamatsu <fuhito615@gmail.com>
2024-06-23 19:11:32 +0900

    Sub

--- apps/pokemon-service/src/commands/pokemon/pokemon.module.ts
+++ apps/pokemon-service/src/commands/pokemon/pokemon.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonController } from './presentation/pokemon.controller';
 import { CreatePokemonCommandHandler } from './application/usecases/create-pokemon.command';
+import { PokemonEventSubscription } from './application/event-subscriptions/pokemon.event-subscription';
 
 @Module({
   imports: [],
   controllers: [PokemonController],
-  providers: [CreatePokemonCommandHandler],
+  providers: [CreatePokemonCommandHandler, PokemonEventSubscription],
 })
 export class PokemonModule {}
