f27bdb61007e762a73e795c772a25d670ccc6995
f-akamatsu <fuhito615@gmail.com>
2024-07-27 13:11:08 +0900

    ValueObject+jest

---
+++ apps/pokemon-service/src/commands/pokemon/domain/value-objects/pokemon-id.spec.ts
@@ -0,0 +1,33 @@
+import { InvalidPokemonException } from '../exceptions/invalid-pokemon.exception';
+import { PokemonId } from './pokemon-id';
+
+describe('PokemonId', () => {
+  describe('バリデーション', () => {
+    it('ObjectIDの形式のときに値が正しいこと', () => {
+      const actual = PokemonId.from('507f1f77bcf86cd799439011');
+      expect(actual.value).toBe('507f1f77bcf86cd799439011');
+    });
+
+    it('16進数以外の文字が使用されているときエラーになること', () => {
+      expect(() => PokemonId.from('507f1f77bcf86cd79943901g')).toThrow(
+        InvalidPokemonException,
+      );
+    });
+
+    it('1文字多いときにエラーになること', () => {
+      expect(() => PokemonId.from('507f1f77bcf86cd7994390110')).toThrow(
+        InvalidPokemonException,
+      );
+    });
+
+    it('1文字少ないときにエラーになること', () => {
+      expect(() => PokemonId.from('507f1f77bcf86cd79943901')).toThrow(
+        InvalidPokemonException,
+      );
+    });
+
+    it('数値のときエラーになること', () => {
+      expect(() => PokemonId.from(0 as never)).toThrow(InvalidPokemonException);
+    });
+  });
+});
--- apps/pokemon-service/src/commands/pokemon/domain/value-objects/pokemon-id.ts
+++ apps/pokemon-service/src/commands/pokemon/domain/value-objects/pokemon-id.ts
@@ -1,13 +1,27 @@
 import { ObjectId } from 'mongodb';
+import { ValueObject } from '../../../../common/domain/value-object';
+import { z } from 'zod';
+import { InvalidPokemonException } from '../exceptions/invalid-pokemon.exception';
 
-export class PokemonId {
-  private constructor(public readonly value: string) {}
+const PokemonIdSchema = z.string().regex(/^[0-9a-f]{24}$/);
 
+export class PokemonId extends ValueObject<string> {
   public static generate(): PokemonId {
     return new PokemonId(new ObjectId().toString()); // TODO mongodbに依存している
   }
 
   public static from(value: string): PokemonId {
     return new PokemonId(value);
+  }
+
+  protected validate(value: string): void {
+    try {
+      PokemonIdSchema.parse(value);
+    } catch (e) {
+      const zodError = e as z.ZodError;
+      throw new InvalidPokemonException(
+        `PokemonId: ${zodError.issues[0].message}`,
+      );
+    }
   }
 }
