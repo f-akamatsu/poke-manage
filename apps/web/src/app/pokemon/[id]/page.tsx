cf8487feffc749136469d73e97eee8c97ca0314e
f-akamatsu <fuhito615@gmail.com>
2024-08-11 21:29:38 +0900

    ポケモン詳細画面一回消す

--- apps/web/src/app/pokemon/[id]/page.tsx
+++ apps/web/src/app/pokemon/[id]/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { Pokemon } from '@/components/organisms/Pokemon/Pokemon';
 import { useParams } from 'next/navigation';
 
 export default function PokemonPage() {
   const params = useParams();
   const idParam = params.id;
   const pokemonId = Array.isArray(idParam) ? idParam[0] : idParam;
-  return <Pokemon pokemonId={pokemonId} />;
+  return (
+    <>
+      🚧 工事中... 🚧 <br />
+      pokemonId: {pokemonId}
+    </>
+  );
 }
--- apps/web/src/components/organisms/Pokemon/Pokemon.schema.ts
+++
@@ -1,9 +0,0 @@
-import { pokedexNoSchema, pokemonNameSchema } from '@/common/common-schema';
-import { z } from 'zod';
-
-export const pokemonFormSchema = z.object({
-  name: pokemonNameSchema,
-  pokedexNo: pokedexNoSchema,
-});
-
-export type PokemonFormSchemaType = z.infer<typeof pokemonFormSchema>;
