ce74ec9d57cdee4aac6bff65e029ec1ec37508a6
f-akamatsu <fuhito615@gmail.com>
2024-11-15 23:37:17 +0900

    一回コミット

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ja'>
+    <html lang='ja' suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
           <Header />
--- apps/web/src/components/organisms/TypeSelect/TypeSelectPresenter.tsx
+++ apps/web/src/components/organisms/TypeSelect/TypeSelectPresenter.tsx
@@ -1,5 +1,12 @@
-import { SelectContent, SelectItem, SelectRoot } from '@/components/ui/select';
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from '@/components/ui/select';
 import { createListCollection } from '@chakra-ui/react';
+import { useRef } from 'react';
 
 export interface TypeSelectOptions {
   label: string;
@@ -24,22 +31,28 @@ export function TypeSelectPresenter({
   isInvalid,
 }: TypeSelectPresenterProps) {
   const collection = createListCollection({ items: types });
+  const contentRef = useRef<HTMLDivElement>(null);
   return (
-    <SelectRoot
-      name={name}
-      value={value}
-      onValueChange={({ value }) => onChange(value)}
-      onInteractOutside={onBlur}
-      collection={collection}
-      width='220px'
-    >
-      <SelectContent>
-        {collection.items.map((type) => (
-          <SelectItem item={type} key={type.value}>
-            {type.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </SelectRoot>
+    <div ref={contentRef}>
+      <SelectRoot
+        name={name}
+        value={value}
+        onValueChange={({ value }) => onChange(value)}
+        onInteractOutside={onBlur}
+        collection={collection}
+        width='220px'
+      >
+        <SelectTrigger>
+          <SelectValueText placeholder='タイプを選択' />
+        </SelectTrigger>
+        <SelectContent portalRef={contentRef}>
+          {collection.items.map((type) => (
+            <SelectItem item={type} key={type.value}>
+              {type.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </SelectRoot>
+    </div>
   );
 }
