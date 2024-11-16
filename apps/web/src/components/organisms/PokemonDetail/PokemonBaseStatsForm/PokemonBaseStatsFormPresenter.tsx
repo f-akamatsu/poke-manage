718cade45afc2a450b5bab9baddaa2371e2b32fe
f-akamatsu <fuhito615@gmail.com>
2024-11-16 23:53:22 +0900

    レイアウト修正

--- apps/web/src/components/organisms/PokemonDetail/PokemonBaseStatsForm/PokemonBaseStatsFormPresenter.tsx
+++ apps/web/src/components/organisms/PokemonDetail/PokemonBaseStatsForm/PokemonBaseStatsFormPresenter.tsx
@@ -1,4 +1,16 @@
-import { Flex, Grid, GridItem, Icon, IconButton, Tooltip } from '@chakra-ui/react';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Icon,
+  IconButton,
+  Spacer,
+  Tooltip,
+} from '@chakra-ui/react';
 import { useFormContext } from 'react-hook-form';
 import { FaFloppyDisk, FaPenToSquare } from 'react-icons/fa6';
 import NumberInput from '../../../atoms/NumberInput/NumberInput';
@@ -25,101 +37,108 @@ export function PokemonBaseStatsFormPresenter({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Flex alignItems='center' gap={12}>
-        <Grid
-          templateRows='repeat(3, 1fr)'
-          templateColumns='repeat(2, 1fr)'
-          rowGap={4}
-          columnGap={12}
-        >
-          {/* HP */}
-          <GridItem>
-            <NumberInput
-              {...register('hitPoints', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='HP'
-              errorMessage={errors.hitPoints?.message}
-            />
-          </GridItem>
+      <Card w='fit-content'>
+        <CardHeader>
+          <Flex alignItems='center'>
+            <Heading size='md'>種族値</Heading>
+            <Spacer />
+            {/* それぞれのボタンにkeyをつけないと挙動がおかしくなる */}
+            {isEditing ? (
+              <Tooltip label='変更を保存します'>
+                <IconButton
+                  key='save-button'
+                  type='submit'
+                  aria-label='変更を保存する'
+                  icon={<Icon as={FaFloppyDisk} />}
+                  isLoading={isFetching}
+                  isDisabled={!isValid}
+                  colorScheme='red'
+                />
+              </Tooltip>
+            ) : (
+              <Tooltip label='種族値を変更します'>
+                <IconButton
+                  key='edit-button'
+                  type='button'
+                  aria-label='種族値を変更する'
+                  icon={<Icon as={FaPenToSquare} />}
+                  colorScheme='red'
+                  variant='outline'
+                  onClick={onClickEditIcon}
+                />
+              </Tooltip>
+            )}
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <Grid
+            templateRows='repeat(3, 1fr)'
+            templateColumns='repeat(2, 1fr)'
+            rowGap={4}
+            columnGap={12}
+          >
+            {/* HP */}
+            <GridItem>
+              <NumberInput
+                {...register('hitPoints', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='HP'
+                errorMessage={errors.hitPoints?.message}
+              />
+            </GridItem>
 
-          {/* こうげき */}
-          <GridItem>
-            <NumberInput
-              {...register('attack', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='こうげき'
-              errorMessage={errors.attack?.message}
-            />
-          </GridItem>
+            {/* こうげき */}
+            <GridItem>
+              <NumberInput
+                {...register('attack', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='こうげき'
+                errorMessage={errors.attack?.message}
+              />
+            </GridItem>
 
-          {/* ぼうぎょ */}
-          <GridItem>
-            <NumberInput
-              {...register('defense', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='ぼうぎょ'
-              errorMessage={errors.defense?.message}
-            />
-          </GridItem>
+            {/* ぼうぎょ */}
+            <GridItem>
+              <NumberInput
+                {...register('defense', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='ぼうぎょ'
+                errorMessage={errors.defense?.message}
+              />
+            </GridItem>
 
-          {/* とくこう */}
-          <GridItem>
-            <NumberInput
-              {...register('spAttack', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='とくこう'
-              errorMessage={errors.spAttack?.message}
-            />
-          </GridItem>
+            {/* とくこう */}
+            <GridItem>
+              <NumberInput
+                {...register('spAttack', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='とくこう'
+                errorMessage={errors.spAttack?.message}
+              />
+            </GridItem>
 
-          {/* とくぼう */}
-          <GridItem>
-            <NumberInput
-              {...register('spDefense', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='とくぼう'
-              errorMessage={errors.spDefense?.message}
-            />
-          </GridItem>
+            {/* とくぼう */}
+            <GridItem>
+              <NumberInput
+                {...register('spDefense', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='とくぼう'
+                errorMessage={errors.spDefense?.message}
+              />
+            </GridItem>
 
-          {/* すばやさ */}
-          <GridItem>
-            <NumberInput
-              {...register('speed', { valueAsNumber: true })}
-              isReadOnly={!isEditing}
-              label='すばやさ'
-              errorMessage={errors.speed?.message}
-            />
-          </GridItem>
-        </Grid>
-
-        {/* それぞれのボタンにkeyをつけないと挙動がおかしくなる */}
-        {isEditing ? (
-          <Tooltip label='変更を保存します'>
-            <IconButton
-              key='save-button'
-              type='submit'
-              aria-label='変更を保存する'
-              icon={<Icon as={FaFloppyDisk} />}
-              isLoading={isFetching}
-              isDisabled={!isValid}
-              colorScheme='teal'
-            />
-          </Tooltip>
-        ) : (
-          <Tooltip label='種族値を変更します'>
-            <IconButton
-              key='edit-button'
-              type='button'
-              aria-label='種族値を変更する'
-              icon={<Icon as={FaPenToSquare} />}
-              colorScheme='teal'
-              variant='outline'
-              onClick={onClickEditIcon}
-            />
-          </Tooltip>
-        )}
-      </Flex>
+            {/* すばやさ */}
+            <GridItem>
+              <NumberInput
+                {...register('speed', { valueAsNumber: true })}
+                isReadOnly={!isEditing}
+                label='すばやさ'
+                errorMessage={errors.speed?.message}
+              />
+            </GridItem>
+          </Grid>
+        </CardBody>
+      </Card>
     </form>
   );
 }
